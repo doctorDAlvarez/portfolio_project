e836a96ffcb3d230a7d3bc5b529b5404f9648ffc
Diego Alvarez <doctordalvarez@Diegos-MacBook-Air.local>
2021-05-02 20:35:41 -0300

    updates

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //require necessary modules and json data.
 const express = require('express');
-const data = require('./data');
-const path = require('path');
+const { projects } = require('./data');
 
 //initialize the express app
 const app = express();
@@ -9,20 +8,28 @@ const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-app.get('/', (req, res, next) => {
-    res.render('index', { projects: data.projects})
+app.get('/', (req, res) => {
+    res.render('index', { projects });
 });
 
-app.get('/about', (req, res, next) => {
+app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/project/:id', (req, res, next) => {
-    res.render('project', { project: data.projects[req.params.id]});
+app.get('/project/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId);
+    if ( project ) {
+        res.render('project', { project });
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 //app.use(errorHandlers);
 
 
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server listening on port 3000')
+});
